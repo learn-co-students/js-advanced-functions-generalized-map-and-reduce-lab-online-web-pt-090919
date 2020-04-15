216ead7f127949c268c710fca2cc6d8354792d01
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-15 18:31:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your functions here
+function map(sourceArray, fn){
+ let mappedArray = [] 
+ sourceArray.forEach(index => {
+   mappedArray.push(fn(index))
+ })
+ return mappedArray
+}
+
+function reduce(sourceArray, fn, startingPoint) {
+    let accumulator
+    let i
+
+    if (!!startingPoint) {
+        accumulator = startingPoint
+        i = 0
+    } else {
+        accumulator = fn(sourceArray[0], sourceArray[1])
+        i = 2
+    }
+    for(i; i < sourceArray.length; i++) {
+        accumulator = fn(accumulator, sourceArray[i])
+    }
+    return accumulator
+} 
+ 
+
