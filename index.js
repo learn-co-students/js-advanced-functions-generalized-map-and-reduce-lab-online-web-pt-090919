c992e610be2ce8c5adf33750d30a37b9e7dea773
vanigami <vanigami@github.com>
2020-04-15 19:06:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your functions here
+function map(arr, fn) {
+    let mappedArr = []
+    arr.map(i => {
+        mappedArr.push(fn(i))
+    })
+    return mappedArr
+}
+
+function reduce(arr, fn, start) {
+    let a
+    let b
+    if (!!start) {
+        a = start
+        b = 0
+    } else {
+        a = fn(arr[0], arr[1])
+        b = 2
+    }
+    for (b; b < arr.length; b++) {
+        a = fn(a, arr[b])
+    }
+    return a
+}
