83b18e750cc265e369ec6ae07a461459d945b5f2
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-04-17 04:11:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your functions here
+
+let map = function(source){
+  let result = []
+  for(let i = 0; i < source.length; i++){
+    result.push(source[i] * -1)
+  }
+  return result
+}
+
+function mapCallback (map){
+  result.push(source[i])
+}
