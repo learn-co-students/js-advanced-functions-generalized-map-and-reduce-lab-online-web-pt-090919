58003aeab9efe10a2beaf0ea3dd0db6e066aec9f
fasnimo <fasnimo@gmail.com>
2020-05-01 13:56:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map(arr, fun) {
+    let otherArray = []
+    arr.forEach(index => {
+        otherArray.push(fun(index))
+    })
+    return otherArray
+}
+
+function reduce(arr, fun, start){
+    let accum
+    let init
+    if (!!start) {
+        accum = start
+        init = 0
+    } else {
+        accum = fun(arr[0], arr[1])
+        init = 2
+    }
+    for(init; init < arr.length; init++) {
+        accum = fun(accum, arr[init])
+    }
+    return accum
+}
